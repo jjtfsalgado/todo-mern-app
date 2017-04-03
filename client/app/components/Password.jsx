77f3d981e1bb64de8f728feba5c565dc3dcbeebe
José Salgado <jjtfsalgado@gmail.com>
2017-04-03 18:43:09 +0100

    Corrected some typos

--- client/app/components/Password.jsx
+++ client/app/components/Password.jsx
@@ -152,7 +152,7 @@ var Password = React.createClass({
     if (this.state.submit == 'success') {
       var alert = (
         <Alert bsStyle={this.state.submit}>
-          <strong>Success!</strong> Please click on the link sended to your email.
+          <strong>Success!</strong> Please click on the link sent to your email.
         </Alert>
       )
     }
